c92c880d579175921388062d42b7dff556ec0913
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-16 12:13:20 +0300

    Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -11,15 +11,13 @@ class GithubOrgClient:
         """Fetch org data from GitHub"""
         url = f"https://api.github.com/orgs/{self.org_name}"
         return get_json(url)
-
+    
     @property
     def _public_repos_url(self):
         return self.org.get("repos_url")
-
-    def public_repos(self, license=None):
-        """
-        Return list of public repository names, optionally filtered by license
-        """
+    
+    def public_repos(self):
+        """Return list of public repository names"""
         repos = get_json(self._public_repos_url)
 
         if license:
@@ -27,7 +25,7 @@ class GithubOrgClient:
             return [repo["name"] for repo in licensed_repos]
 
         return [repo["name"] for repo in repos]
-
+    
     @staticmethod
     def has_license(repo, license_key):
         """Check if repo has the given license key"""
