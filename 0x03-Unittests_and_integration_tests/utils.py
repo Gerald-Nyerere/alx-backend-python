6b8785c3465ecd6422b02d535b6284068d71451e
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-16 10:23:51 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -3,6 +3,8 @@ def access_nested_map(nested_map, path):
     """Access a value in a nested dictionary using a tuple path"""
     current = nested_map
     for key in path:
+        if not isinstance(current, dict):
+            raise KeyError(key)
         if key not in current:
             raise KeyError(key)
         current = current[key]
