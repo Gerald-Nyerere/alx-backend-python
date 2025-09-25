e19d3681f7c0ceecc53654b47f0b915f8fd0d15e
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-25 13:34:23 +0300

    Logging User Requests

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -52,7 +52,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
     'chats.middleware.RequestLoggingMiddleware',
+    "chats.middleware.RestrictAccessByTimeMiddleware"
 ]
 
 REST_FRAMEWORK = {
