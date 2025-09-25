2b0a1f2026b0f18241091a184ecb1ad1a8d29b1c
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-25 13:04:44 +0300

    Logging User Requests

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,16 +1,32 @@
 import logging
 from datetime import datetime
+from django.utils.deprecation import MiddlewareMixin
 
-logger = logging.getLogger(__name__)  
 
-class RequestLoggingMiddleware:
-    def __init__(self, get_response):
+logger = logging.getLogger('request_logger')
+logger.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('requests.log')
+file_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(message)s')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+class RequestLoggingMiddleware(MiddlewareMixin):
+    def __init__(self, get_response=None):
         self.get_response = get_response
+        super().__init__(get_response)
 
     def __call__(self, request):
-        user = request.user if request.user.is_authenticated else 'AnonymousUser'
-
-        logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
-
+        user = "Anonymous"
+        if hasattr(request, 'user') and request.user.is_authenticated:
+            user = request.user.username
+        
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logger.info(log_message)
+        
         response = self.get_response(request)
+        
         return response
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -166,18 +166,3 @@ SIMPLE_JWT = {
     "USER_ID_FIELD": "user_id", 
     "USER_ID_CLAIM": "user_id", 
 }
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
-    },
-}
