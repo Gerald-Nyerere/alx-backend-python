047aa4c3621cbd52c96e7a3021b113d8d713ad8b
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-25 13:14:40 +0300

    Logging User Requests

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -52,8 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-     # custom middleware
     'chats.middleware.RequestLoggingMiddleware',
 ]
 
