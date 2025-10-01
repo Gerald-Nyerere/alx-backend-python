87619d18c33ad2bf7fba5112399834143d2a6d97
gerald-nyerer <geraldnyerer100@gmail.com>
2025-10-01 12:18:36 +0300

    Leverage Advanced ORM Techniques for Threaded Conversations

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
+from .models import Message
 # Create your views here.
 
 @login_required
@@ -13,3 +13,12 @@ def delete_user(request):
         request.user.delete()
         return redirect("home")  
     return redirect("profile")  
+
+
+def inbox(request):
+    messages = (
+        Message.objects.filter(parent_message__isnull=True)
+        .select_related("sender", "receiver")
+        .prefetch_related("replies__sender", "replies__receiver")
+    )
+    return render(request, "messaging/inbox.html", {"messages": messages})
