49d569f82bca3c8aa56c461247468d1dd4400da5
gerald-nyerer <geraldnyerer100@gmail.com>
2025-10-01 12:23:55 +0300

    Leverage Advanced ORM Techniques for Threaded Conversations

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Message
+
 # Create your views here.
+@login_required
+def send_message(request, receiver_id, parent_id=None):
+    """
+    Allows the logged-in user to send a message.
+    Optionally links the message as a reply to another.
+    """
+    receiver = get_object_or_404(User, id=receiver_id)
+    parent_message = None
+
+    if parent_id:
+        parent_message = get_object_or_404(Message, id=parent_id)
+
+    if request.method == "POST":
+        content = request.POST.get("content")
+        if content:
+            Message.objects.create(sender=request.user, receiver=receiver, content=content, parent_message=parent_message)
+            return redirect("inbox")  
+
+    return render(request, "messaging/send_message.html", {"receiver": receiver, "parent_message": parent_message })
 
 @login_required
 def delete_user(request):
