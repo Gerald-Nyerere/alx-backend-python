da410754c4a4dcbe4d42ef3d217f255ccd0951f8
gerald-nyerer <geraldnyerer100@gmail.com>
2025-10-01 12:03:05 +0300

    Create a Signal for Logging Message Edits

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -8,6 +8,7 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name="edited_messages")
 
     class Meta:
         ordering = ['-timestamp'] 
